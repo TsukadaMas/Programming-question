33f6e4a07783e7390126cb6e41e25bc114dbe9ca
Mas Tsukada <46246538+TsukadaMas@users.noreply.github.com>
2023-01-19 12:23:42 -0500

    Update test.js

--- test.js
+++ test.js
@@ -2,15 +2,28 @@
 var input = [ ["L4", "123 kings road" , 2022], [ "L1", "123 kings road" , 2020], ["L2" , "20 queen road" ,1990 ], ["L3", "20 queen road" , 1992] ];
 
 
-function returnMostRecent(input){
-    var mostrecent = [0,0,0];
-    for(var address in input){
-        if(input[address][2] >= mostrecent[2]){
-            mostrecent = input[address];
+function ReturnMostRecent(input){
+    var mostrecent = [];
+    for(var record in input){
+      if(mostrecent.some(address => address.includes(input[record][1]))){
+        var mostRecentRecord = FindAddress(mostrecent, input[record]);
+        if(mostrecent[mostRecentRecord][2] <  input[record][2]){
+            mostrecent[mostRecentRecord] = input[record];
         }
+      }else{
+        mostrecent.push(input[record]);
+      }
     }
     return mostrecent;
 } 
 
 
-console.log(returnMostRecent(input));
+console.log(ReturnMostRecent(input));
+
+function FindAddress(mostrecent, record){
+    for(var i=0; i<mostrecent.length; i++){
+        if(mostrecent[i][1] == record[1]){
+            return i;
+        }
+    }
+}
